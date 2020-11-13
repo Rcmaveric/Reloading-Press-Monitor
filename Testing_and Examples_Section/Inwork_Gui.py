68429f4ded529e2d4b15ef05f1b186a9feea986f
Ryan Conner <71806966+Rcmaveric@users.noreply.github.com>
2020-11-13 10:25:30 -0500

    starting work cams
    Now the Export and clear buttons work. Working on the cam buttons now

--- Testing_and Examples_Section/Inwork_Gui.py
+++ Testing_and Examples_Section/Inwork_Gui.py
@@ -19,6 +19,7 @@ from PIL import Image, ImageTk
 from ttkthemes import ThemedStyle
 import csv
 import datetime
+import cv2
 
 #Created a Load data class
 class Current_Load_Data():
@@ -38,16 +39,60 @@ class Current_Load_Data():
         self.round_count = round_count
 
 class Cam1(tk.Toplevel):
-    def __init__(self, master):
-        tk.Toplevel.__init__(self, master)
-        self.master = master
-        #Path has to be exact to logo image
-        image = Image.open("GUI_Working/primer.jpg")
-        #Resize loaded image.
-        render = ImageTk.PhotoImage(image)
-        self.img = tk.Label(self, image=render)
-        self.img.image = render #may seam silly... but this is so that the photo stays stored in memory, if not Python may trash it
-        self.img.pack()
+    def __init__(self, window, window_title, video_source=0):
+        tk.Toplevel.__init__(self, window, window_title, video_source)
+        self.window = window
+        self.window.title("Cam1")
+        self.video_source = video_source
+
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = MyVideoCapture(self.video_source)
+ 
+        # Create a canvas that can fit the above video source size
+        self.canvas = tk.Canvas(window, width = self.vid.width, height = self.vid.height)
+        self.canvas.pack()
+ 
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 15
+        self.update()
+ 
+            
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
+
+        self.window.after(self.delay, self.update)
+
+class MyVideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+
+        # Get video source width and height
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
 
 class Cam2(tk.Toplevel):
     def __init__(self, master):
