0d1cc5cbea47999a51e8f38ad0cf35526417c873
Rcmaveric <rcmaveric112@gmail.com>
2020-12-04 14:07:42 -0500

    Finnal some success with the alerts

--- Testing_and Examples_Section/Alarms_Test.py
+++ Testing_and Examples_Section/Alarms_Test.py
@@ -2,12 +2,16 @@ import gpiozero as gp
 import tkinter as tk
 import time
 from tkinter import ttk
+import threading
 
 
 S4 = gp.Button(24) #Binding Sensor
-Ldr3 = gp.LightSensor(19) #Case Feeder 
+Ldr1 = gp.LightSensor(4) #Low Primer
+Ldr2 = gp.LightSensor(19) #Bullet Feeder 
+Ldr3 = gp.LightSensor(26) #Case Feeder
 Buzzer = gp.PWMOutputDevice(18, frequency=6000)
-Relay2 = gp.OutputDevice(6, initial_value=False, active_high=True)
+Relay1 = gp.OutputDevice(6, initial_value=False, active_high=True)#Bullets
+Relay2 = gp.OutputDevice(12, initial_value=False, active_high=True)#Cases
 
 class Alarm (tk.LabelFrame):
     def __init__(self, master):
@@ -15,11 +19,21 @@ class Alarm (tk.LabelFrame):
         self.master = master
         
     #Alerts
-      #Binding
-        binding_warning = ttk.LabelFrame(self, text="Binding")
-        binding_warning.pack(side = "left") 
-        binding_label = tk.Button(binding_warning, text="Good", background="green", height=2, width=5)
-        binding_label.pack(fill = "both")
+    #Low Bullets
+        low_bullet_warning = ttk.LabelFrame(self,text="Bullets")
+        low_bullet_warning.pack(side = "left")
+        low_bullet_label = tk.Button(low_bullet_warning, text="Good", background="green", height=2,
+                                     width=5, command = self.Low_Bullet_Reset)
+        low_bullet_label.pack(fill = "both")
+        Relay1.source = Ldr2
+
+      #Low Primers
+        low_primer_warning = ttk.LabelFrame(self, text="Primers")
+        low_primer_warning.pack(side = "left")
+        self.low_primer_label =  tk.Button(low_primer_warning, text="Good", background="green",
+                                            height=2, width=5, command= self.Low_Primer_Reset)
+        self.low_primer_label.pack(fill = "both")
+        Ldr1.when_light= self.Low_Primer_Alert
 
       #Low Cases
         low_case_warning = ttk.LabelFrame(self, text="Bullets")
@@ -29,9 +43,10 @@ class Alarm (tk.LabelFrame):
                                         height=2, width=5)
         self.low_case_label.pack(fill = "both")
         Relay2.source = Ldr3
-        
+        self.T1 = threading.Thread(target=self.Low_Cases, daemon=True)
+        self.T1.start()
 
-    def Low_Cases(self): #Doesnt work.
+    def Low_Cases(self): #Works But doesnt quit thread when exiting main loop
         while True:
             pulsetime = 0
             Ldr3.wait_for_light()
@@ -39,16 +54,34 @@ class Alarm (tk.LabelFrame):
             while Ldr3.light_detected == True:
                 time.sleep(0.2)
                 pulsetime += 1
-                print (pulsetime)
+                print ("Case Timer = " + str(pulsetime))
                 if pulsetime >= 15:
-                    Buzzer.pulse()
                     self.low_case_label.configure(bg="red", text = "Alert")
+                    Buzzer.pulse(n=1)
+                if Ldr3.light_detected == False:
+                    pass
+    
+    def Low_Primer_Alert(self):
+        startTime = time.time()
+        if Ldr1.light_detected == True:
+            endTime = time.time()
+            if (endTime - startTime > 3):
+                Buzzer.pulse()
+                self.low_primer_label.configure(bg="red", text = "Alert")
+                print("Low Primers")
         
     def Low_Cases_Reset(self):
         Buzzer.off()
         self.low_case_label.configure(bg="green", text = "Good")
-
+    
+    def Low_Primer_Reset(self):
+        self.low_primer_label.configure(bg="green", text = "Good")
+        Buzzer.off()
         
+    def Low_Bullet_Reset(self):
+        self.low_bullet_label.configure(bg="green", text = "Good")
+        Buzzer.off()
+
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
